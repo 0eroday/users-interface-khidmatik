959e6544d00ebc0f18d20fb428ada8355f5b132c
Mohammed kasem <mohamedtarbosh1@gmail.com>
2019-06-26 01:41:41 +0300

    update/chang style for the about component

--- src/components/layout/Home_about_section.js
+++ src/components/layout/Home_about_section.js
@@ -2,28 +2,34 @@ import React from 'react'
 
 const Home_about_section = () => {  
   return (
-    <div className="wrapper">
-        <div>
-          <i className="fas fa-handshake fa-3x"></i>
-          <h2>Our Trusted Services </h2>     
+    <div className="about">
+      <div className="box">
+        <i className="icon fas fa-handshake fa-3x"></i>
+        <div className="content">
+          <h3>Our Trusted Services </h3>     
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia iusto cum reprehenderit molestiae quasi, magnam corporis ut quos exe
-            </p>
+          </p>
         </div>
-        <div>
-          <i className="fas fa-cogs fa-3x"></i>
-          <h2> Technical Support </h2>
+      </div>
+      <div className="box">
+        <i className="icon fas fa-cogs fa-3x"></i>
+        <div className="content">
+          <h3> Technical Support </h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nam, distinctio dolor tempora repellat architecto nostrum. Aliquam vero,
           </p>
         </div>
-        <div>
-        <i className="fas fa-lock fa-3x"></i>
-        <h2> Your Privacy Is Our Priority</h2>
+      </div>
+      <div className="box">
+        <i className="icon fas fa-lock fa-3x"></i>
+        <div className="content">
+          <h3> Your Privacy Is Our Priority</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, quo quam quos sapiente atque possimus laborum,
           </p>
         </div>
+      </div>
     </div>
   )
 }
